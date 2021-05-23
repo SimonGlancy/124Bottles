2e3751934d0e4254d58b671afcf7de121f01f3bd
Simon Glancy <simon@jigsaw.xyz>
2021-05-23 17:21:46 +0100

    added animation for number

--- App.tsx
+++ App.tsx
@@ -154,10 +154,17 @@ const AnimatedPintTotal = (props: { color: string; number: number }) => {
   const totalAnimation = useRef(new Animated.Value(0)).current;
   const animateTotal = () =>
     Animated.timing(totalAnimation, {
-      toValue: 1,
-      duration: 500,
+      toValue: 0,
+      duration: 0,
       useNativeDriver: true,
-    }).start();
+    }).start(() =>
+      Animated.timing(totalAnimation, {
+        toValue: 1,
+        duration: 500,
+        useNativeDriver: true,
+        easing: Easing.elastic(1),
+      }).start()
+    );
 
   useEffect(() => {
     console.log('______here', number);
@@ -166,7 +173,22 @@ const AnimatedPintTotal = (props: { color: string; number: number }) => {
 
   const containerTransform = totalAnimation.interpolate({
     inputRange: [0, 1],
-    outputRange: [200, 0],
+    outputRange: [60, 0],
+  });
+
+  const containerTransformX = totalAnimation.interpolate({
+    inputRange: [0, 0.25, 0.5, 0.75, 1],
+    outputRange: [0, -2, 0, 2, 0],
+  });
+
+  const containerGrow = totalAnimation.interpolate({
+    inputRange: [0, 0.8, 1],
+    outputRange: [0.5, 1.1, 1],
+  });
+
+  const textTransform = totalAnimation.interpolate({
+    inputRange: [0, 1],
+    outputRange: [-200, 0],
   });
   return (
     <Animated.View
@@ -180,10 +202,21 @@ const AnimatedPintTotal = (props: { color: string; number: number }) => {
         left: '33.3%',
         alignItems: 'center',
         justifyContent: 'center',
-        transform: [{ translateY: containerTransform }],
+        transform: [
+          { translateY: containerTransform },
+          { translateX: containerTransformX },
+          { scale: containerGrow },
+        ],
       }}
     >
-      <Text style={styles.pintTotal}>{number}</Text>
+      <Animated.Text
+        style={[
+          styles.pintTotal,
+          { transform: [{ translateY: textTransform }] },
+        ]}
+      >
+        {number}
+      </Animated.Text>
     </Animated.View>
   );
 };
