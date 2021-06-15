47d42d03a145b1b59ff03a93e37c6068eda458c2
Emmapr123 <emmapriester@hotmail.com>
2021-06-15 14:44:21 +0100

    Extracted most styles from general information

--- src/components/general-pint-stats/general-pint-stats.tsx
+++ src/components/general-pint-stats/general-pint-stats.tsx
@@ -3,6 +3,7 @@ import {
   Dimensions,
   Text,
   View,
+  StyleSheet
 } from 'react-native';
 import { PintProps } from '../pint';
 
@@ -12,14 +13,43 @@ const GeneralPintStats = ({pint}: {pint: PintProps }) => {
 
   return(
     <View style={{justifyContent: 'flex-end', height: height * 0.2, width, backgroundColor: 'white', position: 'absolute', bottom: 0, left: 0, borderRadius: 10}}>
-      <View style={{top: 10, position: 'absolute'}}>
-      <Text style={{fontSize: 24, fontWeight: 'bold', left: 10 }}>{pint.name}</Text>
-      <Text style={{fontSize: 12, left: 10, top: 1}}>Information per pint</Text>
+      <View style={styles.header}>
+      <Text style={styles.title}>{pint.name}</Text>
+      <Text style={styles.information}>Information per pint</Text>
       </View>
-      <Text style={{fontSize: 18, padding: 20, top: 50, position: 'absolute'}}>{pint.calories} kCal</Text>
-      <Text style={{fontSize: 18, padding: 20, top: 80, position: 'absolute'}}>{pint.units} units</Text>
+      <Text style={styles.calories}>{pint.calories} kCal</Text>
+      <Text style={styles.units}>{pint.units} units</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  header: {
+    top: 10, 
+    position: 'absolute'
+  },
+  title: {
+    fontSize: 24, 
+    fontWeight: 'bold', 
+    left: 10 
+  },
+  information: {
+    fontSize: 12, 
+    fontStyle: 'italic',
+    left: 10, top: 1
+  },
+  calories: {
+    fontSize: 18, 
+    padding: 20, 
+    top: 50, 
+    position: 'absolute'
+  },
+  units: {
+    fontSize: 18, 
+    padding: 20, 
+    top: 80, 
+    position: 'absolute'
+  }
+})
 
 export { GeneralPintStats }
