d3faf5199f184aa1042b4176c41b690f96c5a1a3
Emmapr123 <emmapriester@hotmail.com>
2021-06-15 23:47:52 +0100

    Got it to animate in this time

--- src/components/drank-pint-stats/drank-pint-stats.tsx
+++ src/components/drank-pint-stats/drank-pint-stats.tsx
@@ -20,14 +20,14 @@ const DrankPintStats = ( {finishedPints, pints}: { finishedPints: DrankPint[], p
 
     Animated.timing(modalAmiation, {
       toValue: 1,
-      duration: 8000,
-      useNativeDriver: false
-    })
+      duration: 500,
+      useNativeDriver: true
+    }).start()
   }
 
   const scroll = modalAmiation.interpolate({
-    inputRange: [0, 0.5, 1],
-    outputRange: [1, height, newHeight]
+    inputRange: [0, 1],
+    outputRange: [height, 0]
   })
 
   useEffect(() => {
@@ -35,7 +35,7 @@ const DrankPintStats = ( {finishedPints, pints}: { finishedPints: DrankPint[], p
   })
 
   return(
-    <View style={{justifyContent: 'flex-end', height: height * 0.6, width, backgroundColor: 'white', position: 'absolute', bottom: 0, left: 0}}>
+    <Animated.View style={{justifyContent: 'flex-end', height: height * 0.6, width, backgroundColor: 'white', position: 'absolute', bottom: 0, left: 0, right: 0, transform: [{translateY: scroll}]}}>
       <View style={{flexDirection: 'row', padding: 10, position: 'absolute', top: 10, width}}> 
         <Text style={{position: 'absolute', top: 10, fontWeight: 'bold', fontSize: 24, padding: 10}} >Consumption:</Text>
         <Text style={{position: 'absolute', top: 25, right: 10}}> Since {dayjs(finishedPints[0].dateDrank).format('MM/DD/YY')}</Text>
@@ -51,15 +51,15 @@ const DrankPintStats = ( {finishedPints, pints}: { finishedPints: DrankPint[], p
       const amountOfUnits = Math.round(item.units * amountDrank)
 
       return(
-        <Animated.View key={index} style={{top: height * 0.1, flexDirection: 'row', justifyContent: 'space-between', margin: 10, transform: [{translateX: scroll}], height: newHeight}}>
+        <View key={index} style={{top: height * 0.1, flexDirection: 'row', justifyContent: 'space-between', margin: 10, height: newHeight}}>
           <Text style={{position: 'absolute', right: 10, top: 2}} >{amountOfCalories} kCal / {amountOfUnits} units</Text>
           <View style={{backgroundColor: `${item.color}`, position:'relative', width: width * (percentage / 100)}}/>
           <Text style={{alignSelf: 'center', fontSize: 24, color: 'gray', position: 'absolute', marginLeft: 10}}>{item.name}</Text>
-        </Animated.View>
+        </View>
       )
     }}
     />
-    </View>
+    </Animated.View>
   )
 }
 
